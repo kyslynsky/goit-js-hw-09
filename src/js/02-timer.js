88f1cc075eb0e7efab44dfe44b5b9e931fa6bee3
kyslynsky <kyslynsky@gmail.com>
2022-05-09 17:33:54 +0300

    problem

---
+++ src/js/02-timer.js
@@ -0,0 +1,81 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+};
+
+let intervalId = null;
+
+refs.startBtn.setAttribute('disabled', true);
+
+const config = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    let selectedUserDate = selectedDates[0].getTime();
+    checkValidDate(selectedUserDate);
+    getTimerValues(selectedUserDate);
+  },
+};
+
+flatpickr('#datetime-picker', config);
+
+function checkValidDate(date) {
+  if (date < config.defaultDate) {
+    alert('Please choose a date in the future');
+    refs.startBtn.setAttribute('disabled', true);
+    return;
+  }
+
+  refs.startBtn.removeAttribute('disabled');
+}
+
+function getTimerValues(date) {
+  const resultTime = date - config.defaultDate;
+  const time = convertMs(resultTime);
+
+  if (resultTime > 0) {
+    updateClockFace(time);
+  }
+}
+
+function updateClockFace({ days, hours, minutes, seconds }) {
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+refs.startBtn.addEventListener('click', () => {
+  intervalId = setInterval(getTimerValues, 1000);
+});
